c19e5d22242b43887f847e73920d2dfd4aa91d9e
Joshua Lee <jminlee24+1@outlook.com>
2024-10-24 13:27:04 -0400

    new rotation matrix because the matrix exponential is confusing me

--- contrast_max.py
+++ contrast_max.py
@@ -4,6 +4,37 @@ import scipy.linalg as sla
 import bisect
 import config
 
+def get_translation_matrix(t: np.ndarray) -> np.ndarray:
+    return np.array([
+        [1, 0, t[0]],
+        [0, 1, t[1]],
+        [0, 0, 0 ]
+    ])    
+
+def get_rotation_matrix(w: np.ndarray) -> np.ndarray:
+    r = w[0]
+    p = w[1]
+    y = w[2]
+       
+    yaw_mat = np.array([
+        [np.cos(y), -np.sin(y), 0 ],
+        [np.sin(y),  np.cos(y), 0 ],
+        [0        , 0         , 1 ]
+    ])
+
+    pitch_mat = np.array([
+        [ np.cos(p), 0,  np.sin(p)],
+        [0, 1, 0],
+        [-np.sin(p), 0 , np.cos(p)],
+    ])
+
+    roll_mat = np.array([
+       [ 1, 0, 0], 
+       [ 0, np.cos(r), -np.sin(r)], 
+       [ 0, np.sin(r),  np.cos(r)], 
+    ])
+
+    return np.matmul(yaw_mat, np.matmul(pitch_mat, roll_mat)) 
 
 def get_cross_matrix(omega: np.ndarray) -> np.matrix:
     """ returns the skew symmetric matrix related to the cross product with omega
@@ -16,8 +47,8 @@ def get_cross_matrix(omega: np.ndarray) -> np.matrix:
     """
     return np.matrix(
         [[0, -omega[2],  omega[1]],
-         [omega[2], 0, -omega[0]],
-            [omega[1], omega[2], 0]])
+         [omega[2], 0,  -omega[0]],
+         [omega[1], omega[2], 0]])
 
 
 def rot_warp_pixel(e: np.ndarray, t: float, theta: np.ndarray) -> np.ndarray:
@@ -29,19 +60,22 @@ def rot_warp_pixel(e: np.ndarray, t: float, theta: np.ndarray) -> np.ndarray:
     res = res.astype(int)
     return res.astype(int)
 
+def vel_warp_pixel(e: np.ndarray, t:float, v: np.ndarray) -> np.ndarray:
+    x_bar = np.array(e[0], e[1]) + v
+
+    return x_bar.astype(int)
 
 def minimize(f, events, initial_guess):
     return scipy.optimize.minimize(f, initial_guess, args=(events), method="Nelder-Mead")
 
 
 def event_image(events):
-    print(events[-1])
     img = np.zeros((config.IMAGE_HEIGHT, config.IMAGE_WIDTH))
     for event in events:
         # the actual formula per pixel is b_k * s(x - x_k) with s = dirac delta funciton but no
         # we can also use the gaussian with multivar normal dist with mu = x_k (and sigma = 1?)
         event = event.astype(int)
-        if 0 <= event[0] <= config.IMAGE_WIDTH and 0 <= event[1] <= config.IMAGE_HEIGHT:
+        if 0 <= event[0] < config.IMAGE_WIDTH and 0 <= event[1] < config.IMAGE_HEIGHT:
             img[event[1]][event[0]] += event[2]
     return img
 
@@ -60,4 +94,4 @@ def event_window(events, t0, t):
 
 
 def get_initial_guess(_events):
-    return np.array([0., 0., 0.]).flatten()
+    return np.array([0.001, 0.00000, 0.0000]).flatten()
