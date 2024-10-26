eabe06d7c8394a4e9c554df692c1036531511b3b
jminlee24 <jminlee24@outlook.com>
2024-10-25 20:47:44 -0400

    organized everything made stuff nice added readme

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 TEST_RECORDING_PATH = "data/shapes_rotation/events.txt"
 RECORDING_PATH = "./data/recording.hdf5"
-TIME_WINDOW = 500
+TIME_WINDOW = 350
+START_TIME = 700
 IMAGE_WIDTH = 240
 IMAGE_HEIGHT = 180
--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-import numpy as np
-import h5py
-import bisect
-import scipy
-
+import sys
+import argparse
 import config
-import plotter
-import visualization
-import contrast_max as cmax
-import file_handler
 
+import contrast_max
 
-def single_pass(x0, events):
-    warped_events = []
-    for event in events:
-        warped_events.append(cmax.rot_warp_pixel(event, event[3], x0))
-    e_img = cmax.event_image(warped_events)
-    return -cmax.compute_variance(e_img, warped_events)
-
-
-def framework(events, d_t):
-    t0 = events[0][3] + 500
-    e_window: np.ndarray    = cmax.event_window(events, t0, d_t)
-    e_adjusted = np.array(
-        list(map(lambda x: (x[0], x[1], x[2], x[3] - t0), e_window)))
-    x0 = cmax.get_initial_guess(e_adjusted)
-    res = scipy.optimize.minimize(single_pass, x0=x0, args=e_adjusted, method="CG")
-    print(res)
-    warped_events = [cmax.rot_warp_pixel(event, event[3], res.x)
-                 for event in e_adjusted]
-    warped_img = cmax.event_image(warped_events)
-    img = cmax.event_image(e_adjusted)
-    plotter = visualization.Plotter()
-    plotter.plot_image(img, "normal")
-    plotter.plot_image(warped_img, "warped!") 
-    plotter.show()
 
 if __name__ == "__main__":
-    #  plotter.plothdf5(config.RECORDING_PATH)
-    fileHandler = file_handler.FileHandler(config.TEST_RECORDING_PATH)
-    fileHandler.read_file(h=30000)
+    p = argparse.ArgumentParser()
+    p.add_argument("-r", "--read_limit")
+    p.add_argument("-t", "--time_start")
+    args = p.parse_args(sys.argv[1:])
 
-    events = fileHandler.events
-    framework(events, 500)
+    h = -1
+    t0 = config.START_TIME
 
-    # first pass
+    if args.read_limit:
+        h = int(args.read_limit)
+    if args.time_start:
+        t0 = int(args.time_start)
+
+    cm = contrast_max.ContrastMaximizer(config.TEST_RECORDING_PATH, h=h)
+    res = cm.maximize_variance(t0)
+    print(res)
+    cm.plot_images()
